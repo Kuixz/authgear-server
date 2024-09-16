852dc0f3458e3d1324eeb2d22194008417f53bab
Zachary Tsang <u.quixz@gmail.com>
2024-09-16 12:50:02 +0800

    Scaffold verify email page

--- pkg/auth/handler/webapp/authflowv2/routes.go
+++ pkg/auth/handler/webapp/authflowv2/routes.go
@@ -84,8 +84,9 @@ const (
 	// nolint: gosec
 	AuthflowV2RouteSettingsMFAPassword = "/settings/mfa/password"
 
-	AuthflowV2RouteSettingsIdentityListEmail = "/settings/identity/email"
-	AuthflowV2RouteSettingsIdentityViewEmail = "/settings/identity/view_email"
+	AuthflowV2RouteSettingsIdentityListEmail   = "/settings/identity/email"
+	AuthflowV2RouteSettingsIdentityVerifyEmail = "/settings/identity/verify_email"
+	AuthflowV2RouteSettingsIdentityViewEmail   = "/settings/identity/view_email"
 
 	AuthflowV2RouteSettingsIdentityListUsername = "/settings/identity/username"
 )
