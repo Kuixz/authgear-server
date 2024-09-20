3c706ec4b30880def8f5201e3b4d1be041378217
Zachary Tsang <u.quixz@gmail.com>
2024-09-20 18:29:46 +0800

    Sleeker reporting

--- devtools/gotemplatelinter/extraneous.sh
+++ devtools/gotemplatelinter/extraneous.sh
@@ -9,12 +9,21 @@ fi
 # Read the JSON file and extract keys
 keys=$(jq 'paths | map(tostring) | join(".")' "$1")
 
+# Check all found
+all_found=true
+
 # rg
 for key in $keys; do
-  if rg "$key" --type 'html' > /dev/null; then
-    echo -n "       "
-  else
-    echo -n "UNUSED "
+  if ! rg "$key" --type 'html' > /dev/null; then
+    all_found=false
+    echo "UNUSED $key"  # Would be better only to print this if -v is passed. But I don't know how to implement that. doglaugh
   fi
-  echo "$key"
 done
+
+# Exit with error if there is unused key
+if [ "$all_found" = false ]; then
+    echo "Error: Some keys are unused"
+    exit 1
+fi
+
+exit 0
